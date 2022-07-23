4bd869098326b05c191630d594491d155429d31a
DongHun <donghun.kdh@gmail>
2022-07-23 20:31:52 +0900

    모델만들고 학습시키기 (Flatten Layer, Model Summary)

--- 프로젝트2. 이 사진이 바지인지 셔츠인지 구분해보자 (이미지 분류와 CNN)/apparel_classification.py
+++ 프로젝트2. 이 사진이 바지인지 셔츠인지 구분해보자 (이미지 분류와 CNN)/apparel_classification.py
@@ -1,3 +1,4 @@
+from pickletools import optimize
 import tensorflow as tf
 import matplotlib.pyplot as plt
 
@@ -21,3 +22,29 @@ class_names = [
     "Bag",
     "Ankleboot",
 ]
+
+model = tf.keras.Sequential(
+    [
+        tf.keras.layers.Dense(
+            128, input_shape=(28, 28), activation="relu"  # input_shape: input data의 모양
+        ),
+        tf.keras.layers.Dense(64, activation="relu"),
+        tf.keras.layers.Flatten(),  # 다차원 행렬을  1차원으로 압축
+        tf.keras.layers.Dense(
+            len(class_names),  # 마지막 레이어의 노드 수는 카테고리의 개수,
+            activation="softmax",  # softmax: 결과를 0~1로 압축, 카테고리 예측 문제에 사용하는 activation function
+        ),
+    ]
+)
+
+model.summary()  # model의 아웃라인 보여주기
+
+exit()
+
+model.compile(
+    loss="sparse_categorical_crossentropy",  # 카테고리 예측 문제에 쓰이는 loss function
+    optimizer="adam",
+    metrics=["accuracy"],
+)
+
+model.fit(train_x, train_y, epochs=5)
