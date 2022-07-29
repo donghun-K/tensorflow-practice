18c8b00ace0097cc8c5d121f7683862f48b60cc8
DongHun <donghun.kdh@gmail>
2022-07-30 00:33:35 +0900

    Tensor 데이터 직접 전처리하기 & 모델만들기

--- 프로젝트2.5 개와 고양이를 구분하는 AI 만들기 & 이미지 전처리 (이미지 분류와 CNN)/dogs_and_cats.py
+++ 프로젝트2.5 개와 고양이를 구분하는 AI 만들기 & 이미지 전처리 (이미지 분류와 CNN)/dogs_and_cats.py
@@ -30,3 +30,41 @@ val_ds = (
         seed=1234,
     )
 )
+
+
+# 전처리
+def pre_processing(img, y):
+    img = tf.cast(img / 255.0, tf.float32)
+    return (img, y)
+
+
+train_ds = train_ds.map(pre_processing)
+val_ds = train_ds.map(pre_processing)
+
+
+model = tf.keras.Sequential(
+    [
+        tf.keras.layers.Conv2D(
+            32, (3, 3), padding="same", activation="relu", input_shape=(64, 64, 3)
+        ),
+        tf.keras.layers.MaxPooling2D((2, 2)),
+        tf.keras.layers.Conv2D(
+            64, (3, 3), padding="same", activation="relu", input_shape=(64, 64, 3)
+        ),
+        tf.keras.layers.Dropout(0.2),  # 레이어의 20%를 제거, Over fitting 방지
+        tf.keras.layers.MaxPooling2D((2, 2)),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(128, activation="relu"),
+        tf.keras.layers.Dense(1, activation="sigmoid"),
+    ]
+)
+
+model.summary()
+
+model.compile(
+    loss="binary_crossentropy",
+    optimizer="adam",
+    metrics=["accuracy"],
+)
+
+model.fit(train_ds, validation_data=val_ds, epochs=5)
